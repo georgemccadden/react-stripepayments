37ac421f99ddddd56aac0af582ee38c01195489d
George McCadden III <thethirdone.g@gmail.com>
2021-01-30 18:06:12 -0800

    Here goes nothing

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styled from "@emotion/styled";
+import GlobalStyles from "./prebuilt/GlobalStyles";
+
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
+
+// In an effort to reduce load time, I won't call loadStripe within the render funtion.
+
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <GlobalStyles />
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Elements stripe={stripePromise}>{children}</Elements>
+    </>
+  );
+};
+export default Layout;
---
+++ utils/get-donut-price.js
@@ -0,0 +1,3 @@
+const getDonutPrice = (numDonuts) => (Math.round(numDonuts * 1.5 * 100) / 100).toFixed(2);
+
+export default getDonutPrice;
